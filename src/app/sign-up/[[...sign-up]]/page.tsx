108e3806cd6473c2fa8015e32f4928b11c2a6be3
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-12 00:16:03 +0600

    added forced redirect url to sync-user in the env. Now new users are synced in the database

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,11 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <SignUp />
+      <SignUp
+        appearance={{
+          variables: { colorPrimary: "#f97316ff" },
+        }}
+      />
     </div>
   );
 }
