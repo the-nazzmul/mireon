4a5bc2ecfb811788d7919ffa3a8345cedf142183
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-11 17:29:14 +0600

    set up sign-in and sign-up page with clerk and added middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
-  return <h1 className="text-red-600">Hello world</h1>;
+  return <Button>Click me</Button>;
 }
