c9c460e8610a85567f20e163590b04c3a051eeb6
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-11 18:01:07 +0600

    added sync-user page and disabled some annoying rules of eslint

---
+++ src/app/sync-user/page.tsx
@@ -0,0 +1,42 @@
+import { db } from "@/server/db";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+//This is the page where user will be redirected after authenticating with clerk. Here I will sync the user with the DB
+const SyncUser = async () => {
+  // check if there is user
+  const { userId } = await auth();
+
+  //   throw error if there is not user
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const client = await clerkClient();
+
+  const user = await client.users.getUser(userId);
+  if (!user.emailAddresses[0]?.emailAddress) {
+    return notFound();
+  }
+
+  await db.user.upsert({
+    where: {
+      emailAddress: user.emailAddresses[0]?.emailAddress ?? "",
+    },
+    update: {
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    create: {
+      id: userId,
+      emailAddress: user.emailAddresses[0]?.emailAddress ?? "",
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  });
+  return redirect("/dashboard");
+};
+
+export default SyncUser;
