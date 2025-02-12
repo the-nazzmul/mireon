d0d3974f41295da5eaddf838c855aa196f4ea2c2
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-12 18:48:34 +0600

    created the UI for create project page, added project schema in prisma schema

---
+++ src/app/(private)/create/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+type FormInput = {
+  projectName: string;
+  repoUrl: string;
+  githubToken?: string;
+};
+
+const CreatePage = () => {
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+
+  const onSubmit = (data: FormInput) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex h-full items-center justify-center gap-12">
+      <img src="/coding.svg" alt="image" className="h-56 w-auto" />
+      <div>
+        <div>
+          <h1 className="text-2xl font-semibold">
+            Link your GitHub Repository
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter the URL of your repository to link it to Mireon
+          </p>
+        </div>
+        <div className="h-4" />
+
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-2 space-y-2">
+          <Input
+            {...(register("projectName"), { required: true })}
+            placeholder="Project Name"
+            required
+          />
+          <Input
+            {...(register("repoUrl"), { required: true })}
+            placeholder="Github URL"
+            type="url"
+            required
+          />
+          <Input
+            {...register("githubToken")}
+            placeholder="Github Token (Optional)"
+          />
+          <Button type="submit">Create Project</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
