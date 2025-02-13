a8b39d518fefa115c34f7d0a1ce894a8239d9711
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-13 10:05:45 +0600

    wrote a function to get commitHashes. It needs to be refactored to make things dynamic

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -3,3 +3,36 @@ import { Octokit } from "octokit";
 export const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
+
+const githubUrl = "https://github.com/docker/genai-stack";
+
+type Response = {
+  commitHash: string;
+  commitMessage: string;
+  commitAuthorName: string;
+  commitAuthorAvatar: string;
+  commitDate: string;
+};
+
+export const getCommitHashes = async (
+  githubUrl: string,
+): Promise<Response[]> => {
+  const { data } = await octokit.rest.repos.listCommits({
+    owner: "docker",
+    repo: "genai-stack",
+  });
+  const sortedCommits = data.sort(
+    (a: any, b: any) =>
+      new Date(b.commit.author.date).getTime() -
+      new Date(a.commit.author.date).getTime(),
+  ) as any[];
+  return sortedCommits.slice(0, 15).map((commit: any) => ({
+    commitHash: commit?.sha as string,
+    commitMessage: commit?.commit?.message ?? "",
+    commitAuthorName: commit?.commit?.author?.name ?? "",
+    commitAuthorAvatar: commit?.author?.avatar_url ?? "",
+    commitDate: commit?.commit?.author?.date ?? "",
+  }));
+};
+
+console.log(await getCommitHashes(githubUrl));
