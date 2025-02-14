6d6cef36d1b03347d7e9e458f95300817251c56f
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-14 10:38:46 +0600

    added langchain-community langchain/core and wrote a hook to load the documents of a github repo

---
+++ src/hooks/github-loader.ts
@@ -0,0 +1,22 @@
+import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+
+export const loadGithubRepo = async (
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const loader = new GithubRepoLoader(githubUrl, {
+    accessToken: githubToken || "",
+    branch: "main",
+    ignoreFiles: [
+      "package-lock.json, yarm.lock",
+      "pnpm-lock.yaml",
+      "bun.lockb",
+    ],
+    recursive: true,
+    unknown: "warn",
+    maxConcurrency: 5,
+  });
+
+  const docs = await loader.load();
+  return docs;
+};
